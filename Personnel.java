08d5b0e2daaf5d2a9187df87985a00b87fbe0999
Luke <tsitoluke@gmail.com>
2017-01-09 06:49:21 +0100

    first commit

---
+++ Personnel.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class Personnel
+{
+	public int idPersonnel;
+	public String nom;
+	public String prenom;
+	public Date dateDeNaissance;
+	public String sexe;
+}
